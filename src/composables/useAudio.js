1c93f0050ee80ec9dc3b7eaab6c107fe9345a95b
Agustin <agustimassoni11@hotmail.com>
2025-08-20 23:38:33 -0300

    feat: añadir lógica para la gestión de música de fondo en el composable useAudio

---
+++ src/composables/useAudio.js
@@ -0,0 +1,61 @@
+// src/composables/useAudio.js
+import { ref, watch } from "vue";
+import { Howl } from "howler";
+
+// Elimina 'backgroundMusic', ya que no se usa.
+const isMusicPlaying = ref(false);
+const isMusicEnabled = ref(true);
+
+let bgmInstance = null;
+
+const initBackgroundMusic = () => {
+  if (!bgmInstance) {
+    bgmInstance = new Howl({
+      src: [""],
+      html5: true,
+      loop: true,
+      volume: 0.3,
+    });
+  }
+};
+
+const toggleMusic = () => {
+  if (isMusicPlaying.value) {
+    stopMusic();
+  } else {
+    playMusic();
+  }
+};
+
+const playMusic = () => {
+  if (isMusicEnabled.value && bgmInstance && !bgmInstance.playing()) {
+    bgmInstance.play();
+    isMusicPlaying.value = true;
+  }
+};
+
+const stopMusic = () => {
+  if (bgmInstance && bgmInstance.playing()) {
+    bgmInstance.pause();
+    isMusicPlaying.value = false;
+  }
+};
+
+watch(isMusicEnabled, (isEnabled) => {
+  if (isEnabled) {
+    playMusic();
+  } else {
+    stopMusic();
+  }
+});
+
+export default function useAudio() {
+  return {
+    isMusicEnabled,
+    isMusicPlaying,
+    initBackgroundMusic,
+    toggleMusic,
+    playMusic,
+    stopMusic,
+  };
+}
